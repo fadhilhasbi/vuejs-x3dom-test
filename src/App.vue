84e0666b21a8c9a079c62aa64f0bc1564cd739cf
Fadhil Hasbi <muhammad1900016050@webmail.uad.ac.id>
2023-05-15 21:06:14 +0700

    init files

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MyX3DScene from './components/MyX3DScene.vue';
+
+</script>
+
+<template>
+  <MyX3DScene msg="Vue + X3DOM"/>
+</template>
+
+<style scoped>
+/* fill style */
+</style>
---
+++ src/components/MyX3DScene.vue
@@ -0,0 +1,31 @@
+<script setup>
+import 'x3dom'
+import 'x3dom/x3dom.css'
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="container">
+    <p>This is my X3D scene</p>
+    <x3d width='500px' height='400px'>
+      <scene>
+        <shape>
+          <appearance>
+            <material diffuseColor='1 0 0'></material>
+          </appearance>
+          <box></box>
+        </shape>
+      </scene>
+    </x3d>
+  </div>
+</template>
+
+<style scoped>
+/* fill for style */
+</style>
